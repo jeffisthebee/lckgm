6224741ac651b48654bcb0bd43911f7c203a805f
jeffisthebee <jakean13689@gmail.com>
2026-01-16 18:49:21 +0900

    ec 5t

--- src/engine/simEngine.js
+++ src/engine/simEngine.js
@@ -2,6 +2,6 @@
 export * from './rosterLogic';
 export * from './mechanics';
 export * from './draftLogic'; // <--- Changed from draftEngine
-export * from './gameEngine';
+export * from './gameLogic';
 export * from './scheduleLogic';
 export * from './seasonLogic';
