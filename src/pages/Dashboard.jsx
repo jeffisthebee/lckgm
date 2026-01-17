4c10d1648f7d574ea7c707d4a5406a3b1c4f4140
jeffisthebee <jakean13689@gmail.com>
2026-01-17 15:15:55 +0900

    pleaseee

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,8 @@ import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { teams, teamFinanceData } from '../data/teams';
 import { championList, difficulties } from '../data/constants';
-import { simulateMatch, getTeamRoster, generateSchedule, quickSimulateMatch } from '../engine/simEngine';
+// [FIX] Changed import from '../engine/simEngine' to '../engine/gameLogic'
+import { simulateMatch, getTeamRoster, generateSchedule, quickSimulateMatch } from '../engine/gameLogic';
 import LiveGamePlayer from '../components/LiveGamePlayer';
 import DetailedMatchResultModal from '../components/DetailedMatchResultModal';
 import playerList from '../data/players.json';
