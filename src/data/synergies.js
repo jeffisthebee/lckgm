1a505b5bb03ded825a92fa18e44054ff4bb5ca3f
jeffisthebee <jakean13689@gmail.com>
2026-06-05 18:29:08 +0900

    1.1 roadtomsi synergy

--- src/data/synergies.js
+++ src/data/synergies.js
@@ -63,6 +63,17 @@ export const SYNERGIES = [
   { champions: ['트리스타나', '렐'], multiplier: 1.06 },
   { champions: ['직스', '베이가'], multiplier: 1.05 },
   { champions: ['세라핀', '소나'], multiplier: 1.06 },
+  
+  // 새로 추가된 바텀 조합들
+  { champions: ['케이틀린', '브라움'], multiplier: 1.05 }, 
+  { champions: ['이즈리얼', '니코'], multiplier: 1.04 },
+  { champions: ['바루스', '렐'], multiplier: 1.04 },
+  { champions: ['진', '바드'], multiplier: 1.06 },
+  { champions: ['카이사', '렐'], multiplier: 1.05 },
+  { champions: ['이즈리얼', '밀리오'], multiplier: 1.04 },
+  { champions: ['이즈리얼', '세라핀'], multiplier: 1.05 },
+  { champions: ['시비르', '바드'], multiplier: 1.05 },
+  { champions: ['자야', '노틸러스'], multiplier: 1.05 },
 
   // ==========================================
   // 2. TOP & JUNGLE SYNERGIES
@@ -94,6 +105,14 @@ export const SYNERGIES = [
   { champions: ['다리우스', '아이번'], multiplier: 1.06 },
   { champions: ['리븐', '리 신'], multiplier: 1.05 },
   { champions: ['나서스', '스카너'], multiplier: 1.04 },
+  
+  // 새로 추가된 탑-정글 및 상체 주도권 조합들
+  { champions: ['암베사', '녹턴'], multiplier: 1.05 },
+  { champions: ['암베사', '바이'], multiplier: 1.05 },
+  { champions: ['제이스', '마오카이'], multiplier: 1.04 },
+  { champions: ['크산테', '신짜오'], multiplier: 1.06 },
+  { champions: ['럼블', '마오카이'], multiplier: 1.07 },
+  { champions: ['럼블', '스카너'], multiplier: 1.07 },
 
   // ==========================================
   // 3. MID & JUNGLE (Includes Yasuo Comps)
@@ -164,6 +183,15 @@ export const SYNERGIES = [
   { champions: ['베이가', '뽀삐'], multiplier: 1.06 },
   { champions: ['아지르', '자르반 4세'], multiplier: 1.06 },
   { champions: ['빅토르', '아무무'], multiplier: 1.05 },
+  
+  // 새로 추가된 미드-정글 메타 조합들
+  { champions: ['오리아나', '오공'], multiplier: 1.05 },
+  { champions: ['오리아나', '신짜오'], multiplier: 1.05 },
+  { champions: ['애니', '오공'], multiplier: 1.05 },
+  { champions: ['탈리야', '바이'], multiplier: 1.05 },
+  { champions: ['아지르', '뽀삐'], multiplier: 1.06 },
+  { champions: ['애니', '바이'], multiplier: 1.05 },
+  { champions: ['카시오페아', '리 신'], multiplier: 1.04 },
 
   // ==========================================
   // 4. TEAM COMPOSITIONS (3+ Champions)
@@ -212,83 +240,80 @@ export const SYNERGIES = [
   { champions: ['사이온', '질리언', '말자하'], multiplier: 1.08 },
   { champions: ['헤카림', '시비르', '카르마', '제이스'], multiplier: 1.10 },
   { champions: ['애쉬', '리산드라', '세주아니', '브라움'], multiplier: 1.09 },
-  { champions: ['오리아나', '녹턴'], multiplier: 1.07 }, // Ball Delivery System
-  { champions: ['애쉬', '브라움'], multiplier: 1.06 }, // Passive Stacking Synergy
-  { champions: ['애쉬', '세라핀'], multiplier: 1.05 }, // Slow procs Seraphine's Root automatically
-  { champions: ['이즈리얼', '레오나'], multiplier: 1.03 }, // Aggressive Lane
-  { champions: ['카이사', '알리스타'], multiplier: 1.05 }, // CC adds Plasma stacks
-  { champions: ['칼리스타', '니코'], multiplier: 1.07 }, // Ult Delivery (R -> R)
-  { champions: ['미스 포츈', '자르반 4세'], multiplier: 1.06 }, // Cataclysm + Bullet Time
-  { champions: ['헤카림', '유미'], multiplier: 1.08 }, // High Speed Engage
-  { champions: ['트위치', '라칸'], multiplier: 1.05 }, // Stealth Delivery
-  { champions: ['닐라', '유미'], multiplier: 1.06 }, // Heal/Shield Amp + XP Synergy
-  { champions: ['자르반 4세', '직스'], multiplier: 1.05 }, // Cataclysm + Mega Inferno Bomb
-  { champions: ['사미라', '타릭'], multiplier: 1.05 }, // E dash delivers Dazzle stun
+  { champions: ['오리아나', '녹턴'], multiplier: 1.07 }, 
+  { champions: ['애쉬', '브라움'], multiplier: 1.06 }, 
+  { champions: ['애쉬', '세라핀'], multiplier: 1.05 }, 
+  { champions: ['이즈리얼', '레오나'], multiplier: 1.03 }, 
+  { champions: ['카이사', '알리스타'], multiplier: 1.05 }, 
+  { champions: ['칼리스타', '니코'], multiplier: 1.07 }, 
+  { champions: ['미스 포츈', '자르반 4세'], multiplier: 1.06 }, 
+  { champions: ['헤카림', '유미'], multiplier: 1.08 }, 
+  { champions: ['트위치', '라칸'], multiplier: 1.05 }, 
+  { champions: ['닐라', '유미'], multiplier: 1.06 }, 
+  { champions: ['자르반 4세', '직스'], multiplier: 1.05 }, 
+  { champions: ['사미라', '타릭'], multiplier: 1.05 }, 
   { champions: ['우디르', '아이번'], multiplier: 1.06 },
   { champions: ['카시오페아', '트위치'], multiplier: 1.07 }, 
   { champions: ['카시오페아', '티모'], multiplier: 1.06 },
-
-  // Terrain Creation (Wall Condemn/Stun Synergies)
-  { champions: ['베인', '애니비아'], multiplier: 1.06 }, // Condemn into Crystallize
-  { champions: ['뽀삐', '애니비아'], multiplier: 1.06 }, // Heroic Charge into Crystallize
-  { champions: ['베인', '자르반 4세'], multiplier: 1.05 }, // Condemn into Cataclysm
-  { champions: ['뽀삐', '자르반 4세'], multiplier: 1.05 }, // Heroic Charge into Cataclysm
+  { champions: ['베인', '애니비아'], multiplier: 1.06 }, 
+  { champions: ['뽀삐', '애니비아'], multiplier: 1.06 }, 
+  { champions: ['베인', '자르반 4세'], multiplier: 1.05 }, 
+  { champions: ['뽀삐', '자르반 4세'], multiplier: 1.05 }, 
   { champions: ['라이즈', '누누와 윌럼프'], multiplier: 1.09 }, 
   { champions: ['세나', '초가스'], multiplier: 1.06 }, 
   { champions: ['세나', '사이온'], multiplier: 1.05 },
-  { champions: ['케이틀린', '마오카이'], multiplier: 1.05 }, // Root -> Trap
-  { champions: ['케이틀린', '바드'], multiplier: 1.06 }, // Tempered Fate -> Trap under feet
-  { champions: ['진', '스웨인'], multiplier: 1.05 }, // CC Chain (Root + Pull)
-  { champions: ['칼리스타', '케넨'], multiplier: 1.07 }, // Fate's Call -> Slicing Maelstrom
-  { champions: ['오리아나', '알리스타'], multiplier: 1.06 }, // Ball Delivery (Headbutt/Pulverize)
-  { champions: ['킨드레드', '타릭'], multiplier: 1.08 }, // Lamb's Respite + Cosmic Radiance stacking
-  { champions: ['마스터 이', '룰루'], multiplier: 1.07 }, // Classic Funnel Synergy
-  { champions: ['마스터 이', '모르가나'], multiplier: 1.06 }, // Black Shield prevents CC (Yi's weakness)
-  { champions: ['미스 포츈', '소나'], multiplier: 1.06 }, // Crescendo + Bullet Time
-  { champions: ['미스 포츈', '세라핀'], multiplier: 1.05 }, // Encore + Bullet Time
-  { champions: ['야스오', '요네'], multiplier: 1.06 }, // Brother Synergy (Knockup chaining)
-  { champions: ['트리스타나', '레오나'], multiplier: 1.06 }, // Burst All-in
-  { champions: ['드레이븐', '잔나'], multiplier: 1.05 }, // Shield gives AD -> Stronger Axes
-  { champions: ['루시안', '밀리오'], multiplier: 1.06 }, // Range extension + Passive procs // Gap close support for Udyr
-// ==========================================
+  { champions: ['케이틀린', '마오카이'], multiplier: 1.05 }, 
+  { champions: ['케이틀린', '바드'], multiplier: 1.06 }, 
+  { champions: ['진', '스웨인'], multiplier: 1.05 }, 
+  { champions: ['칼리스타', '케넌'], multiplier: 1.07 }, 
+  { champions: ['오리아나', '알리스타'], multiplier: 1.06 }, 
+  { champions: ['킨드레드', '타릭'], multiplier: 1.08 }, 
+  { champions: ['마스터 이', '룰루'], multiplier: 1.07 }, 
+  { champions: ['마스터 이', '모르가나'], multiplier: 1.06 }, 
+  { champions: ['미스 포츈', '소나'], multiplier: 1.06 }, 
+  { champions: ['미스 포츈', '세라핀'], multiplier: 1.05 }, 
+  { champions: ['야스오', '요네'], multiplier: 1.06 }, 
+  { champions: ['트리스타나', '레오나'], multiplier: 1.06 }, 
+  { champions: ['드레이븐', '잔나'], multiplier: 1.05 }, 
+  { champions: ['루시안', '밀리오'], multiplier: 1.06 }, 
+
+  // ==========================================
   // 5. NEWLY ADDED SYNERGIES (Mechanical)
   // ==========================================
-  
-  // Bot Lane & Support (Aggro/Poke/Safety)
-  { champions: ['사미라', '레오나'], multiplier: 1.07 }, // CC Chain for Style Rank
-  { champions: ['사미라', '아무무'], multiplier: 1.06 }, // Curse of the Sad Mummy setup
-  { champions: ['루시안', '유미'], multiplier: 1.05 }, // High mobility + Lightslinger procs
-  { champions: ['루시안', '레오나'], multiplier: 1.06 }, // Burst damage synergy
-  { champions: ['시비르', '룰루'], multiplier: 1.05 }, // Pix on Ricochet + Move speed
-  { champions: ['시비르', '나미'], multiplier: 1.05 }, // Passive speed stacking
-  { champions: ['진', '나미'], multiplier: 1.06 }, // Tidecaller's Blessing slows for W root
-  { champions: ['진', '레오나'], multiplier: 1.05 }, // Lockdown for Curtain Call
-  { champions: ['애쉬', '레오나'], multiplier: 1.06 }, // Chain CC hell
-  { champions: ['애쉬', '밀리오'], multiplier: 1.05 }, // Range extension on slows
-  { champions: ['칼리스타', '블리츠크랭크'], multiplier: 1.08 }, // Hook + Fate's Call safety/engage
-  { champions: ['코그모', '레나타 글라스크'], multiplier: 1.07 }, // Bailout saves hypercarry
-  { champions: ['징크스', '레나타 글라스크'], multiplier: 1.07 }, // Bailout extends fight for Passive
-  { champions: ['자야', '노틸러스'], multiplier: 1.06 }, // Root holds targets for feathers
-  { champions: ['자야', '레오나'], multiplier: 1.06 }, // Lockdown for Bladecaller
-  { champions: ['닐라', '라칸'], multiplier: 1.07 }, // Double Shields + Engage
-  { champions: ['닐라', '카르마'], multiplier: 1.06 }, // Speed/Shield Amp
-  { champions: ['징크스', '브라움'], multiplier: 1.05 }, // Minigun stacks Concussive Blows fast
-  { champions: ['트위치', '브라움'], multiplier: 1.05 }, // Spray and Pray stacks Passive on everyone
-  { champions: ['야스오', '나미'], multiplier: 1.06 }, // Bubble + Tidal Wave
-  { champions: ['야스오', '노틸러스'], multiplier: 1.06 }, // Depth Charge point-click knockup
-  { champions: ['야스오', '초가스'], multiplier: 1.05 }, // Rupture
-  { champions: ['야스오', '자르반 4세'], multiplier: 1.06 }, // EQ Knockup
-  { champions: ['야스오', '아트록스'], multiplier: 1.05 }, // Q Knockups
-  { champions: ['질리언', '다리우스'], multiplier: 1.08 }, // Speed up the Juggernaut
-  { champions: ['질리언', '올라프'], multiplier: 1.07 }, // Ragnarok + Speed
-  { champions: ['질리언', '나서스'], multiplier: 1.06 }, // Wither + Time Warp
-  { champions: ['케인', '유미'], multiplier: 1.07 }, // Blue Kayn Taxi service
-  { champions: ['베이가', '알리스타'], multiplier: 1.06 }, // Headbutt into Event Horizon
-  { champions: ['베이가', '베인'], multiplier: 1.06 }, // Condemn into Event Horizon
-  { champions: ['스웨인', '판테온'], multiplier: 1.06 }, // Point-click Stun -> Passive Pull
-  { champions: ['다이애나', '오리아나'], multiplier: 1.08 }, // Moonfall pulls -> Shockwave
-  { champions: ['렐', '오리아나'], multiplier: 1.08 }, // Magnet Storm -> Shockwave
-  { champions: ['렐', '미스 포츈'], multiplier: 1.09 }, // Magnet Storm -> Bullet Time
-  { champions: ['렐', '케넨'], multiplier: 1.09 }, // Magnet Storm -> Slicing Maelstrom
-  { champions: ['타릭', '리 신'], multiplier: 1.07 }, // Invisible Stun delivery (W->W)
+  { champions: ['사미라', '레오나'], multiplier: 1.07 }, 
+  { champions: ['사미라', '아무무'], multiplier: 1.06 }, 
+  { champions: ['루시안', '유미'], multiplier: 1.05 }, 
+  { champions: ['루시안', '레오나'], multiplier: 1.06 }, 
+  { champions: ['시비르', '룰루'], multiplier: 1.05 }, 
+  { champions: ['시비르', '나미'], multiplier: 1.05 }, 
+  { champions: ['진', '나미'], multiplier: 1.06 }, 
+  { champions: ['진', '레오나'], multiplier: 1.05 }, 
+  { champions: ['애쉬', '레오나'], multiplier: 1.06 }, 
+  { champions: ['애쉬', '밀리오'], multiplier: 1.05 }, 
+  { champions: ['칼리스타', '블리츠크랭크'], multiplier: 1.08 }, 
+  { champions: ['코그모', '레나타 글라스크'], multiplier: 1.07 }, 
+  { champions: ['징크스', '레나타 글라스크'], multiplier: 1.07 }, 
+  { champions: ['자야', '노틸러스'], multiplier: 1.06 }, 
+  { champions: ['자야', '레오나'], multiplier: 1.06 }, 
+  { champions: ['닐라', '라칸'], multiplier: 1.07 }, 
+  { champions: ['닐라', '카르마'], multiplier: 1.06 }, 
+  { champions: ['징크스', '브라움'], multiplier: 1.05 }, 
+  { champions: ['트위치', '브라움'], multiplier: 1.05 }, 
+  { champions: ['야스오', '나미'], multiplier: 1.06 }, 
+  { champions: ['야스오', '노틸러스'], multiplier: 1.06 }, 
+  { champions: ['야스오', '초가스'], multiplier: 1.05 }, 
+  { champions: ['야스오', '자르반 4세'], multiplier: 1.06 }, 
+  { champions: ['야스오', '아트록스'], multiplier: 1.05 }, 
+  { champions: ['질리언', '다리우스'], multiplier: 1.08 }, 
+  { champions: ['질리언', '올라프'], multiplier: 1.07 }, 
+  { champions: ['질리언', '나서스'], multiplier: 1.06 }, 
+  { champions: ['케인', '유미'], multiplier: 1.07 }, 
+  { champions: ['베이가', '알리스타'], multiplier: 1.06 }, 
+  { champions: ['베이가', '베인'], multiplier: 1.06 }, 
+  { champions: ['스웨인', '판테온'], multiplier: 1.06 }, 
+  { champions: ['다이애나', '오리아나'], multiplier: 1.08 }, 
+  { champions: ['렐', '오리아나'], multiplier: 1.08 }, 
+  { champions: ['렐', '미스 포츈'], multiplier: 1.09 }, 
+  { champions: ['렐', '케넨'], multiplier: 1.09 }, 
+  { champions: ['타릭', '리 신'], multiplier: 1.07 }, 
 ];
